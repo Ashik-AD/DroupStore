77a129854c2831285e96046e50e39856dc9f0cd1
Ashik-AD <looge321@gmail.com>
2021-05-02 18:05:33 +0545

    added: get favorite item from userItemProvider

--- src/components/favourite/Favourite.js
+++ src/components/favourite/Favourite.js
@@ -1,29 +1,23 @@
 import React, { Component } from 'react'
 import Space from '../header/Space'
-import Storage from '../../utils/Storage';
 import ProductLIst from '../productsUI/ProductLIst';
 import ProductItem from '../productsUI/ProductItem';
 import ProductHeader from '../productsUI/ProductHeader';
+import { UserItemContext } from '../../context/UserItemProvider';
 class Favourite extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            favouriteItem: []
-        }
-    }
-    componentDidMount() {
-        const getFavouriteItem = new Storage('fav').getItems();
-        this.setState({ favouriteItem: getFavouriteItem });
-    }
     render() {
         return (
             <section className="container">
                 <Space />
                 <ProductHeader top="Your Liked" sub="Cloath" pos="center" />
                 <ProductLIst column="col-4">
-                    {
-                        this.state.favouriteItem.map(el => <ProductItem key={el.id} {...el}/>)
-                    }
+                    <UserItemContext.Consumer>
+                        {
+                            ({ favItem }) => (
+                                favItem.map(el => <ProductItem key={el.id} {...el}/>)
+                            )
+                        }
+                    </UserItemContext.Consumer>
                 </ProductLIst>
             </section>
         )
