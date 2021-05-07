7008067dd1b642c447e171469bbc19b39f74f33b
Ashik-AD <looge321@gmail.com>
2021-05-07 15:41:46 +0545

    add clear button

--- src/components/favourite/Favourite.js
+++ src/components/favourite/Favourite.js
@@ -4,23 +4,36 @@ import ProductLIst from '../productsUI/ProductLIst';
 import ProductItem from '../productsUI/ProductItem';
 import ProductHeader from '../productsUI/ProductHeader';
 import { UserItemContext } from '../../context/UserItemProvider';
+import ButtonRemove from '../UI/Remove';
 class Favourite extends Component {
+    state = {
+        favItem: []
+    }
+    componentDidMount() {
+        const value = this.context;
+        this.setState({favItem: value.favItem})
+    }
+    handleClearFavorite = () => {
+        this.setState({ favItem: [] });
+    }
     render() {
         return (
-            <section className="container">
+            <section className="container favorite">
                 <Space />
                 <ProductHeader top="Your Liked" sub="Cloath" pos="center" />
+                <div style={{position: 'relative'}}>
+                    <ButtonRemove label="favorite" handleClearPrevState={this.handleClearFavorite}>
+                        Clear Your Favorite
+                    </ButtonRemove>
+                </div>
                 <ProductLIst column="col-4">
-                    <UserItemContext.Consumer>
-                        {
-                            ({ favItem }) => (
-                                favItem.map(el => <ProductItem key={el.id} {...el}/>)
-                            )
-                        }
-                    </UserItemContext.Consumer>
+                    {
+                        this.state.favItem.map(el => <ProductItem key={el.id} {...el}/>)
+                    }
                 </ProductLIst>
             </section>
         )
     }
 }
+Favourite.contextType = UserItemContext;
 export default Favourite
